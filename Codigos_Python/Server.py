f2dc03d0a77fa079de9332ec9c48328dd9b32c79
Dariella06 <dariella.llosa.galarza.2006@gmail.com>
2025-01-21 14:54:45 +0100

    Mejorando el mensaje

--- Codigos_Python/Server.py
+++ Codigos_Python/Server.py
@@ -59,7 +59,7 @@ def getUser():
 
 @app.route('/prototip/getuser/<string:username>', methods=['GET'])
 def prototipGetuser(username):
-    return "Prototip 1: user:" + username
+    return "Prototip 1 - User:" + username
 
 if __name__ == '__main__':
      app.run(debug=True,host="0.0.0.0",port="10050")
