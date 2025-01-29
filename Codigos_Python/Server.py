204206c46cea69302b3b789c9c5539f2d1af61b1
Dariella06 <dariella.llosa.galarza.2006@gmail.com>
2025-01-29 12:17:01 +0100

    la , del user

--- Codigos_Python/Server.py
+++ Codigos_Python/Server.py
@@ -14,7 +14,7 @@ listUsers= [
     User(1,"usuari1", "12345", "prova@gmail.com"),
     User(2,"user2", "123", "user2@proven.cat"),
     User(3,"admin","12","admin@proven.cat"),
-    User(4,"admin2","12")
+    User(4,"admin2","12"),
     User(5,"Dariella","2006","dariella@gmail.com")
 ]
 
