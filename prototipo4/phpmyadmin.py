af972c0d54e7edc31a1533525c7e04c526b09635
Dariella06 <150277460+Dariella06@users.noreply.github.com>
2025-04-22 14:45:05 +0200

    Add files via upload

---
+++ prototipo4/phpmyadmin.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+class DAOUser:
+    def __init__(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="root",
+                database="Tapatapp"
+            )
+            self.cursor = self.connection.cursor(dictionary=True)
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            self.connection = None
+            self.cursor = None
+
+    def get_all_users(self):
+        if not self.cursor:
+            return None
+        query = "SELECT * FROM User"
+        try:
+            self.cursor.execute(query)
+            return self.cursor.fetchall() 
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
+
+    def close_connection(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+#NO BORRAR(SOY IMBECIL Y SE ME OLVIDA LO QUE HICE)
+if __name__ == "__main__":
+    dao = DAOUser()
+    users = dao.get_all_users()
+    if users:
+        for user in users:
+            print(user)
+    dao.close_connection()
